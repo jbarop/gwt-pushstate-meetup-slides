6ca03fff6fae027dfc1dbcd3551f6613fb89203a
Johannes Barop <jb@barop.de>
2013-06-02 01:51:45 +0200

    Layout, Meta information etc

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'HTML5 History API for GWT',
+    subtitle: 'Silky URLs in your application',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
@@ -19,12 +19,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Johannes Barop',
+    company: 'Freelancing web application developer',
+    gplus: 'http://bit.ly/jbarop',
+    twitter: '@jobarop',
+    www: '',
+    github: 'http://github.com/jbarop'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
